a226b4c63746cafdf82010d1acd5a0e41ccc5eda
chrispyduck <chris@compucounts.com>
2020-12-12 22:09:28 -0500

    eslint-disable for ISensor.ts

--- src/ISensor.ts
+++ src/ISensor.ts
@@ -1,5 +1,7 @@
 import { HomieDevice } from "@chrispyduck/homie-device";
 
+/* eslint-disable semi */ // seems to be a bug
+
 export default interface ISensor {
   register(device: HomieDevice): void;
 }
